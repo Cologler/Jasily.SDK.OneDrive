54dd4c0f784bf3a85898adcf4cd8a6d274a617b5
赖巍 <skyoflw@gmail.com>
2015-07-24 03:29:13 +0800

    added scopes

---
+++ Jasily.SDK.OneDrive/Authentications/AuthenticationPermissions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Jasily.SDK.OneDrive.Authentications
+{
+    [Flags]
+    public enum AuthenticationPermissions
+    {
+        /// <summary>
+        /// can not use this.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Allows your application to take advantage of single sign-on capabilities.
+        /// </summary>
+        WindowsLiveSignIn = 1,
+
+        /// <summary>
+        /// Allows your application to receive a refresh token so it can work offline even when the user isn't active. This scope is not available for token flow.
+        /// </summary>
+        WindowsLiveOfflineAccess = 2,
+
+        /// <summary>
+        /// Grants read-only permission to all of a user's OneDrive files, including files shared with the user.
+        /// </summary>
+        OnedriveReadonly = 4,
+
+        /// <summary>
+        /// Grants read and write permission to all of a user's OneDrive files, including files shared with the user. To create sharing links, this scope is required.
+        /// </summary>
+        OnedriveReadwrite = 8,
+
+        /// <summary>
+        /// Grants read and write permissions to a specific folder for your application.
+        /// </summary>
+        OnedriveAppfolder = 16
+    }
+}
---
+++ Jasily.SDK.OneDrive/Authentications/AuthenticationPermissionsExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jasily.SDK.OneDrive.Authentications
+{
+    public static class AuthenticationPermissionsExtensions
+    {
+        public static string GetString(this AuthenticationPermissions permission)
+        {
+            switch (permission)
+            {
+                case AuthenticationPermissions.WindowsLiveSignIn:
+                    return "wl.signin";
+                case AuthenticationPermissions.WindowsLiveOfflineAccess:
+                    return "wl.offline_access";
+                case AuthenticationPermissions.OnedriveReadonly:
+                    return "onedrive.readonly";
+                case AuthenticationPermissions.OnedriveReadwrite:
+                    return "onedrive.readwrite";
+                case AuthenticationPermissions.OnedriveAppfolder:
+                    return "onedrive.appfolder";
+
+                case AuthenticationPermissions.None:
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(permission), permission, null);
+            }
+        }
+
+        /// <summary>
+        /// return like 'wl.signin wl.offline_access onedrive.readwrite'
+        /// </summary>
+        /// <param name="permissions"></param>
+        /// <returns></returns>
+        public static string GetParameterValue(AuthenticationPermissions permissions)
+        {
+            if (permissions == AuthenticationPermissions.None)
+                throw new ArgumentException("can not use None.", nameof(permissions));
+
+            return string.Join(" ", GetSubPermissions(permissions).Select(z => GetString(z)));
+        }
+
+        private static IEnumerable<AuthenticationPermissions> GetSubPermissions(AuthenticationPermissions permissions)
+        {
+            if (permissions.HasFlag(AuthenticationPermissions.WindowsLiveSignIn))
+                yield return AuthenticationPermissions.WindowsLiveSignIn;
+            if (permissions.HasFlag(AuthenticationPermissions.WindowsLiveOfflineAccess))
+                yield return AuthenticationPermissions.WindowsLiveOfflineAccess;
+            if (permissions.HasFlag(AuthenticationPermissions.OnedriveReadonly))
+                yield return AuthenticationPermissions.OnedriveReadonly;
+            if (permissions.HasFlag(AuthenticationPermissions.OnedriveReadwrite))
+                yield return AuthenticationPermissions.OnedriveReadwrite;
+            if (permissions.HasFlag(AuthenticationPermissions.OnedriveAppfolder))
+                yield return AuthenticationPermissions.OnedriveAppfolder;
+        }
+    }
+}
