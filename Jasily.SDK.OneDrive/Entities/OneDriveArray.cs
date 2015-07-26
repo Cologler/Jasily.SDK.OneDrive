9836c88d8d404dbfa09406881b6f0aff50202cab
赖巍 <skyoflw@gmail.com>
2015-07-27 03:13:04 +0800

    added next page method.

--- Jasily.SDK.OneDrive/Entities/OneDriveArray.cs
+++ Jasily.SDK.OneDrive/Entities/OneDriveArray.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,11 @@ namespace Jasily.SDK.OneDrive.Entities
         [DataMember(Name = "value")]
         public List<T> Value { get; set; }
 
+        [DataMember(Name = "@odata.nextLink")]
+        public string NextPageUrl { get; set; }
+
+        public bool HasNext() => !this.NextPageUrl.IsNullOrWhiteSpace();
+
         internal override void SetCreatorController(OneDriveWebController controller)
         {
             base.SetCreatorController(controller);
@@ -24,6 +30,20 @@ namespace Jasily.SDK.OneDrive.Entities
                     item.SetCreatorController(controller);
                 }
             }
+        }
+
+        /// <summary>
+        /// if controller is null, use CreatorController
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <returns></returns>
+        public async Task<WebResult<OneDriveArray<T>>> GetNextAsync(OneDriveWebController controller = null)
+        {
+            if (!this.HasNext())
+                throw new InvalidOperationException("can not get next page.");
+
+            return await (controller ?? this.CreatorController)
+                .RawGetAsync<OneDriveArray<T>>(this.NextPageUrl);
         }
     }
 }
