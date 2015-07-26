55b546edcb9f32a25914404b4fdc7f72d31707d5
赖巍 <skyoflw@gmail.com>
2015-07-27 03:11:45 +0800

    fixed a time parse bug.

--- Jasily.SDK.OneDrive/Entities/OneDriveObject.cs
+++ Jasily.SDK.OneDrive/Entities/OneDriveObject.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.Serialization;
 
 namespace Jasily.SDK.OneDrive.Entities
 {
+    [DataContract]
     public abstract class OneDriveObject
     {
         public DateTime GetISODateTime(string value)
         {
+            if (value.IsNullOrWhiteSpace())
+                throw new FormatException($"{nameof(value)} IsNullOrWhiteSpace().");
+
+            if (value.Length < 20)
+                throw new FormatException($"{nameof(value)} length < 20.");
+
+
             switch (value.Length)
             {
-                case 23:
-                    return DateTime.ParseExact(value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss.ff'Z'", CultureInfo.InvariantCulture);
-                case 24:
-                    return DateTime.ParseExact(value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss.fff'Z'", CultureInfo.InvariantCulture);
+                case 20:
+                    return DateTime.ParseExact(value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'", CultureInfo.InvariantCulture);
+                case 21:
+                    throw new FormatException($"{nameof(value)}.Length == 21.");
                 default:
                     return DateTime.ParseExact(value, $"yyyy'-'MM'-'dd'T'HH':'mm':'ss.{'f'.Repeat(value.Length - 21)}'Z'", CultureInfo.InvariantCulture);
             }
